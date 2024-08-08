e4b911094ed249b412f9f5e69f3a1360e85ce7d1
Prince <156288214+Prince5632@users.noreply.github.com>
2024-08-08 15:45:13 +0530

    python updated notes

---
+++ 01_main.py
@@ -0,0 +1,18 @@
+def sayPrince():
+    print("Hello Prince")
+    print("Hello Prince")
+    print("Hello Prince")
+    print("Hello Prince")
+
+
+sayPrince()
+
+for x in range(0,11):
+    print(x)
+
+sayPrince()
+
+for x in range(0,6):
+    print(x)
+
+sayPrince()
